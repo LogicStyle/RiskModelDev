ef86b0fdbb36f84619539236e0fd010ec6daaa8f
Andrew <taoaming1001@163.com>
2017-06-12 18:31:05 +0800

    update

--- nloptrdemo.R
+++ nloptrdemo.R
@@ -1,10 +1,49 @@
 
 
-RebDates <- getRebDates(as.Date('2010-01-31'),as.Date('2017-05-31'),'month')
-TS <- getTS(RebDates,'EI000985')
 
-gf.NP_stat <- function(TS,Nbin=lubridate::years(-3),type=c('simple','lm')){
+
+#' TS.getFinStat_ts
+#'
+#' get stats of financial indicators from tinysoft.
+#' @param TS
+#' @param funchar see \link[QDataGet]{TS.getFin_rptTS}
+#' @param Nbin
+#' @param changerate
+#' @param stattype
+#' @examples 
+#' RebDates <- getRebDates(as.Date('2013-01-31'),as.Date('2017-05-31'),'month')
+#' TS <- getTS(RebDates,'EI000905')
+#' funchar <- '"factorscore",LastQuarterData(RDate,46078,0)'
+#' 
+#' 
+#' funchar <- '"eps",LastQuarterData(RDate,9900000,0)'
+TS.getFinStat_ts <- function(TS,funchar,varname = funchar,Nbin=lubridate::years(-3),changerate=FALSE,
+                       stattype=c('mean','slope','sd','mean/sd','slope/sd')){
   type <- match.arg(type)
+  stattype <- match.arg(stattype)
+  
+  getrptDate <- function(begT,endT,type=c('between','forward','backward')){
+    type <- match.arg(type)
+    
+    tmp <- seq(begT,endT,by='day')
+    if(type=='forward'){
+      tmp <- lubridate::floor_date(tmp, "quarter")-lubridate::days(1)
+    }else if(type=='backward'){
+      tmp <- lubridate::ceiling_date(tmp, "quarter")-lubridate::days(1)
+    }else{
+      tmp <- c(lubridate::floor_date(tmp, "quarter")-lubridate::days(1),
+               lubridate::ceiling_date(tmp, "quarter")-lubridate::days(1))
+      
+    }
+    
+    rptDate <- sort(unique(tmp))
+    if(type=='between'){
+      rptDate <- rptDate[rptDate>=begT]
+      rptDate <- rptDate[rptDate<=endT]
+    }
+    return(rptDate)
+  }
+  
   
   #get report date 
   begT <- trday.offset(min(TS$date),Nbin)
@@ -23,88 +62,64 @@ gf.NP_stat <- function(TS,Nbin=lubridate::years(-3),type=c('simple','lm')){
   rptTS <- dplyr::left_join(rptTS,tmp,by='stockID')
   rptTS <- rptTS[rptTS$rptDate>rptTS$IPODay,c('rptDate','stockID')]
  
-  funchar <- '"np_belongto_parcomsh",LastQuarterData(RDate,46078,0)'
+  
   TSFdata <- rptTS.getFin_ts(rptTS,funchar)
+  factorname <- colnames(TSFdata)[3]
+  colnames(TSFdata) <- c("rptDate","stockID","factorscore")
   
   rtndata <- TSFdata %>% dplyr::group_by(stockID) %>%
-    dplyr::mutate(growth =(np_belongto_parcomsh - dplyr::lag(np_belongto_parcomsh, 4))/abs(dplyr::lag(np_belongto_parcomsh, 4)))
+    dplyr::mutate(growth =(factorscore - dplyr::lag(factorscore, 4))/abs(dplyr::lag(factorscore, 4)))
+  rtndata <- na.omit(rtndata)
+  rtndata <- rtndata[!is.infinite(rtndata$growth),c("rptDate","stockID","growth")]
   
   
   TSnew <- getrptDate_newest(TS)
   TSnew <- na.omit(TSnew)
   TSnew <- dplyr::rename(TSnew,rptDateEnd=rptDate)
   TSnew$rptDateBeg <- TSnew$rptDateEnd %m+% Nbin
-  
   tmp <- dplyr::distinct(TSnew,rptDateBeg,rptDateEnd)
   tmp <- tmp %>% dplyr::rowwise() %>% 
     dplyr::do(rptDateBeg=.$rptDateBeg,rptDateEnd=.$rptDateEnd,rptDate = getrptDate(.$rptDateBeg, .$rptDateEnd,type = 'between')) %>% 
     dplyr::do(data.frame(rptDateBeg=.$rptDateBeg,rptDateEnd=.$rptDateEnd,rptDate = .$rptDate))
-  
   TSnew <- dplyr::full_join(TSnew,tmp,by=c('rptDateBeg','rptDateEnd'))
   TSnew <- transform(TSnew,rptDateBeg=NULL,rptDateEnd=NULL)
   
-  TSFdata <- dplyr::left_join(TSnew,rtndata[,c("rptDate","stockID","growth")],by=c('stockID','rptDate'))
+  TSFdata <- dplyr::left_join(TSnew,rtndata,by=c('stockID','rptDate'))
   TSFdata <- na.omit(TSFdata)
-  TSFdata <- TSFdata[!is.infinite(TSFdata$growth),]
+  
   TSFdata <- TSFdata %>% dplyr::group_by(date,stockID) %>% dplyr::mutate(id =row_number())
-  
   N <- max(TSFdata$id)
-  TSF <- TSFdata %>% group_by(date,stockID) %>% filter(max(id) > N/2) %>%  summarise(factorscore=mean(growth)/sd(growth))
-  TSF <- left_join(TS,TSF,by=c('date','stockID'))
-  
-  TSF <- TSFdata %>% group_by(date,stockID) %>% filter(max(id) > N/2) %>%  summarise(factorscore=mean(growth))
-  TSF <- left_join(TS,TSF,by=c('date','stockID'))
-  TSF <- RFactorModel:::factor.std(TSF,factorStd = 'sectorNe',sectorAttr = list(std=list(factorList1,33),level=list(5,1)))
-  TSFR <- getTSR(TSF)
-  chart.IC(TSFR)
-  chart.Ngroup.spread(TSFR)
-  table.Ngroup.overall(TSFR)
-}
+  TSFdata <- TSFdata %>% dplyr::group_by(date,stockID) %>% dplyr::filter(max(id) > N/2) 
+  if(stattype=='mean'){
+    TSF <- TSFdata %>%  dplyr::summarise(factorscore=mean(growth))
+  }else if(stattype=='sd'){
+    TSF <- TSFdata %>%  dplyr::summarise(factorscore=sd(growth))
+  }else if(stattype=='mean/sd'){
+    TSF <- TSFdata %>%  dplyr::summarise(factorscore=mean(growth)/sd(growth))
+  }else if(stattype %in% c('slope','slope/sd')){
+    tmp <- TSFdata[1:10000,] %>% do(mod = lm(growth ~ id, data = .))
+    tmp <- data.frame(tmp %>% broom::tidy(mod))
+    TSF <- tmp[tmp$term=='id',c("date","stockID","estimate")]
+    if(stattype=='slope'){
+      colnames(TSF) <- c("date","stockID","factorscore")
+    }else{
+      tmp <- TSFdata %>%  dplyr::summarise(sd=sd(growth))
+      TSF <- dplyr::left_join(TSF,tmp,by=c('date','stockID'))
+      TSF$factorscore <- TSF$estimate/TSF$sd
+      TSF <- transform(TSF,estimate=NULL,sd=NULL)
+    }
 
-
-getrptDate <- function(begT,endT,type=c('between','forward','backward')){
-  type <- match.arg(type)
-  
-  tmp <- seq(begT,endT,by='day')
-  if(type=='forward'){
-    tmp <- lubridate::floor_date(tmp, "quarter")-lubridate::days(1)
-  }else if(type=='backward'){
-    tmp <- lubridate::ceiling_date(tmp, "quarter")-lubridate::days(1)
-  }else{
-    tmp <- c(lubridate::floor_date(tmp, "quarter")-lubridate::days(1),
-             lubridate::ceiling_date(tmp, "quarter")-lubridate::days(1))
     
   }
-
-  rptDate <- sort(unique(tmp))
-  if(type=='between'){
-    rptDate <- rptDate[rptDate>=begT]
-    rptDate <- rptDate[rptDate<=endT]
-  }
-  return(rptDate)
+  
+  
+  TSF <- dplyr::left_join(TS,TSF,by=c('date','stockID'))
+  return(TSF)
 }
 
 
 
-gf.EPS_stat <- function(TS,Nbin='3 years'){
-  #get report date 
-  rptDate <- trday.offset(min(TS$date),Nbin)
-  rptDate <- getRebDates(rptDate,max(TS$date))
-  rptDate <- unique(lubridate::floor_date(rptDate, "quarter")-lubridate::days(1))
-  rptTS <- expand.grid(rptDate = rptDate, stockID = unique(TS$stockID),
-                       KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE)
-  rptTS <- dplyr::arrange(rptTS,rptDate,stockID)
-  
-  #remove report dates before IPO 
-  tmp <- data.frame(date=max(TS$date),stockID = unique(TS$stockID),stringsAsFactors=FALSE)
-  tmp <- TS.getTech_ts(tmp, funchar="Firstday()",varname='IPODay')
-  tmp <- transform(tmp,date=NULL,IPODay=tsdate2r(IPODay))
-  rptTS <- dplyr::left_join(rptTS,tmp,by='stockID')
-  rptTS <- rptTS[rptTS$rptDate>rptTS$IPODay,c('rptDate','stockID')]
-  
-  funchar <- '"eps",LastQuarterData(RDate,9900000,0)'
-  TSFdata2 <- rptTS.getFin_ts(rptTS,funchar)
-  
-  
-}
+
+
+
 
